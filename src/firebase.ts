295504055a1f5f88b7e60fd05f1f7efeefda2bea
Calvin Meiring <calvinmeiring1@gmail.com>
2026-08-08 12:48:32 +0200

    Push latest website updates

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics, isSupported, logEvent } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTWFlqJSemJ-1X5fX_tBVrMWJljlMVQV4",
-  authDomain: "the-sitters-de4cb.firebaseapp.com",
-  projectId: "the-sitters-de4cb",
-  storageBucket: "the-sitters-de4cb.firebasestorage.app",
-  messagingSenderId: "852917602224",
-  appId: "1:852917602224:web:b17e2976b82c2d0a9f3875",
-  measurementId: "G-WJPQYWQSY0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
